451429259b179dd131e9683c314c0e87cef10aaf
Brandon Ashby <69142310+Braash@users.noreply.github.com>
2021-10-25 14:23:03 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-modeule.exports = {
-    target : 'serverless'
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  },
 }
